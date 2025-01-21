2dc2e3ef0e6348f8c509dbf38d1997c776256c4c
Cash-Eze2 <amoscash14@gmail.com>
2025-01-21 10:22:38 +0000

    controller

---
+++ controllers/answer.controller.js
@@ -0,0 +1,52 @@
+const Answer = require('../models/answer');
+const CustomError = require('../utils/CustomError');
+
+class AnswerController {
+  createAnswer = async (req, res, next) => {
+      const answerData = req.body;
+          try {
+                const answerExist = await Answer.findOne({
+                        question: answerData.question,
+                                student: answerData.student,
+                                        class: answerData.class,
+                                              });
+                                                    if (answerExist)
+                                                            throw new CustomError(
+                                                                      'You have already answered this question, consider to update the answer instead!',
+                                                                                400
+                                                                                        );
+
+                                                                                              const answer = await Answer.create(answerData);
+                                                                                                    return res.status(200).json(answer);
+                                                                                                        } catch (error) {
+                                                                                                              console.error(`Error creating answer: ${error}`);
+                                                                                                                    next(error);
+                                                                                                                        }
+                                                                                                                          };
+
+                                                                                                                            updateAnswer = async (req, res, next) => {
+                                                                                                                                const { id } = req.params;
+                                                                                                                                    const data = req.body;
+                                                                                                                                        try {
+                                                                                                                                              const updatedAnswer = await Answer.findByIdAndUpdate(id, data, { new: true });
+                                                                                                                                                    if (!updatedAnswer) throw new CustomError('Answer id you provided not found', 404);
+                                                                                                                                                          return res.status(200).json(updatedAnswer);
+                                                                                                                                                              } catch (error) {
+                                                                                                                                                                    console.error(`Error updating answer: ${error}`);
+                                                                                                                                                                          next(error);
+                                                                                                                                                                              }
+                                                                                                                                                                                };
+
+                                                                                                                                                                                  getAnswers = async (req, res, next) => {
+                                                                                                                                                                                      try {
+                                                                                                                                                                                            const answers = await Answer.find();
+                                                                                                                                                                                                  
+                                                                                                                                                                                                        return res.status(200).json(answers);
+                                                                                                                                                                                                            } catch (error) {
+                                                                                                                                                                                                                  console.error(`Error getting answers: ${error}`);
+                                                                                                                                                                                                                        next(error);
+                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                              };
+                                                                                                                                                                                                                              }
+
+                                                                                                                                                                                                                              module.exports = AnswerController;
---
+++ controllers/datacount.controller.js
@@ -0,0 +1,70 @@
+const Student = require('../models/student');
+const Staff = require('../models/staff');
+const Event = require('../models/event');
+const Course = require('../models/course');
+const Exam = require('../models/exam');
+const Result = require('../models/result');
+const { getGraphData } = require('../utils/utils.datacount');
+
+const dataCountController = async (req, res, next) => {
+  try {
+      const students = await Student.countDocuments();
+          const staff = await Staff.countDocuments();
+              const events = await Event.countDocuments();
+
+                  res.json({ students, staff, events });
+                    } catch (error) {
+                        console.error('Error counting data:', error);
+                            next(error);
+                              }
+                              };
+
+                              const graphDataController = async (req, res, next) => {
+                                try {
+                                    const graphData = await getGraphData();
+
+                                        res.json(graphData);
+                                          } catch (error) {
+                                              console.error('Error getting graph data:', error);
+                                                  next(error);
+                                                    }
+                                                    };
+
+                                                    const staffDatacountController = async (req, res, next) => {
+                                                      const { staff, _class } = req.body;
+                                                        try {
+                                                            const students = await Student.find({ _class: _class }).countDocuments();
+                                                                const courses = await Course.find({ staff: staff, _class: _class }).countDocuments();
+                                                                    const exams = await Exam.find({ staff: staff, _class: _class }).countDocuments();
+                                                                        res.json({
+                                                                              students,
+                                                                                    courses,
+                                                                                          exams,
+                                                                                              });
+                                                                                                } catch (error) {
+                                                                                                    console.error('Error counting data:', error);
+                                                                                                        next(error);
+                                                                                                          }
+                                                                                                          };
+
+                                                                                                          const studentDatacountController = async (req, res, next) => {
+                                                                                                            const { _class, student } = req.body;
+                                                                                                              try {
+                                                                                                                  const courses = await Course.find({ _class }).countDsocuments();
+                                                                                                                      const exams = await Result.find({ _class: _class, student: student }).countDocuments();
+                                                                                                                          res.json({
+                                                                                                                                courses,
+                                                                                                                                      exams,
+                                                                                                                                          });
+                                                                                                                                            } catch (error) {
+                                                                                                                                                console.error('Error counting data:', error);
+                                                                                                                                                    next(error);
+                                                                                                                                                      }
+                                                                                                                                                      };
+
+                                                                                                                                                      module.exports = {
+                                                                                                                                                        dataCountController,
+                                                                                                                                                          graphDataController,
+                                                                                                                                                            staffDatacountController,
+                                                                                                                                                              studentDatacountController,
+                                                                                                                                                              };
