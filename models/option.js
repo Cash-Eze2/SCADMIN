f64d99c551c0a6ba13ad25775f10582127f4c8d5
Cash-Eze2 <amoscash14@gmail.com>
2025-01-21 10:53:16 +0000

    models for each feature

---
+++ models/option.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const optionSchema = new mongoose.Schema({
+  content: {
+      type: String,
+          required: true,
+            },
+              is_correct: {
+                  type: Boolean,
+                      required: true,
+                        },
+                          question: { type: mongoose.Schema.Types.ObjectId, ref: 'Question', required: true },
+                          });
+
+                          const OptionModel = mongoose.model('Option', optionSchema);
+                          module.exports = OptionModel;
