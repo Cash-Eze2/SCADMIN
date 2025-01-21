81576bb1bb44fa07c4d8a67954f44c172c7aa0cf
Cash-Eze2 <amoscash14@gmail.com>
2025-01-21 10:55:05 +0000

    models for a feature

---
+++ models/question.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+  content: {
+      type: String,
+          required: true,
+            },
+              points: {
+                  type: Number,
+                      required: true,
+                        },
+                          exam: { type: mongoose.Schema.Types.ObjectId, ref: 'Exam', required: true },
+                            options: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Option' }],
+                            });
+
+                            const QuestionModel = mongoose.model('Question', questionSchema);
+                            module.exports = QuestionModel;
