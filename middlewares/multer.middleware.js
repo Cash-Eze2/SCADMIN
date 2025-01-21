ae764a69af2c277c7545dbbe83cca44f6db02a0d
Cash-Eze2 <amoscash14@gmail.com>
2025-01-21 10:32:40 +0000

    rendering for a verification push

---
+++ middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+const multer = require('multer');
+
+const allowedImageTypes = ['imagejpeg', 'image/png', 'image/jpg', 'image/gif','image/svg+xml' ];
+const imageFilter = (req, file, cb) => {
+  if (allowedImageTypes.includes(file.mimetype)) cb(null, true);
+    else cb(null, false);
+    };
+    const imageStorage = multer.diskStorage({});
+    const multerMiddleware = multer({
+      storage: imageStorage,
+        fileFilter: imageFilter,
+        }).single('file');
+
+        module.exports = multerMiddleware;
