a5d3f0017a3dc5fb3e2625a88af56e562cc5874b
Cash-Eze2 <amoscash14@gmail.com>
2025-01-21 11:20:21 +0000

    Routes to each feature

---
+++ routes/timetable.routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const TimetableController = require('../controllers/timetable.controller');
+const { authorize } = require('../middlewares/authorize');
+const { validateToken } = require('../middlewares/validations');
+
+const router = express.Router();
+const timetableController = new TimetableController();
+
+router.post(
+  '/timetable',
+    validateToken,
+      authorize(['createTimetable']),
+        timetableController.createTimetable
+        );
+
+        router.get(
+          '/timetable/staff/:id',
+            validateToken,
+              authorize(['getTimetable']),
+                timetableController.getTimetableByStaff
+                );
+
+                router.put(
+                  '/timetable/update',
+                    validateToken,
+                      authorize(['updateTimetable']),
+                        timetableController.updateTimetable
+                        );
+
+                        router.put(
+                          '/timetable/remove',
+                            validateToken,
+                              authorize(['deleteTimetable']),
+                                timetableController.removeTimetable
+                                );
+
+                                module.exports = router;
